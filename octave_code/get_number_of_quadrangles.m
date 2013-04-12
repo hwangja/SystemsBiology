b278362f6a6c53d4170a257eb1a5f7c2be9d5f0b
James Hwang <jameshwang@outlook.com>
2013-04-12 10:36:57 -0400

    fixed triangle and quadrangle

--- octave_code/get_number_of_quadrangles.m
+++ octave_code/get_number_of_quadrangles.m
@@ -5,6 +5,6 @@ function quadrangles = get_number_of_quadrangles(A)
 	# This function finds the number of quadrangles in an adjacency matrix
 	# It uses the equation Q = (trace(A^4)-4*wedges-2*edges)/8
 
-	T4 = trace(A.^4);
+	T4 = trace(A^4);
 	quadrangles = (T4-4*get_number_of_wedges(A)-2*get_number_of_edges(A))/8;
 end
